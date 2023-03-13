953a3c37942003489c9518788f8bfcc856ae0af5
747058 <747058@student.dusd.net>
2023-03-13 11:41:23 -0700

    did scripts and added animals, player, and food

---
+++ Unit2Hernandez/Assets/Scripts/spawnmanager.cs
@@ -0,0 +1,28 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour
+{
+    public GameObject[] animalPrefrabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S));
+        {
+
+            int animalIndex = Random.Range(0, animalPrefrabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefrabs[animalIndex], spawnPos, animalPrefrabs[animalIndex].transform.rotation);
+        }
+    }
+}

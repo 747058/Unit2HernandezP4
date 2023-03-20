59f6e7fa66beea6c57e921b4fc9acdf5754c12c9
747058 <747058@student.dusd.net>
2023-03-20 11:42:00 -0700

    finished project

--- Unit2Hernandez/Assets/Scripts/Detectcollisions.cs
+++ Unit2Hernandez/Assets/Scripts/Detectcollisions.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moveforward : MonoBehaviour
+public class Detectcollisions : MonoBehaviour
 {
-    public float speed = 10.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,12 @@ public class Moveforward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
--- Unit2Hernandez/Assets/Scripts/spawnmanager.cs
+++ Unit2Hernandez/Assets/Scripts/spawnmanager.cs
@@ -7,22 +7,28 @@ public class spawnmanager : MonoBehaviour
     public GameObject[] animalPrefrabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Randomly generate animal index and spawn position 
-            int animalIndex = Random.Range(0, animalPrefrabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefrabs[animalIndex], spawnPos, animalPrefrabs[animalIndex].transform.rotation);
-        }
     }
+
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefrabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefrabs[animalIndex], spawnPos, animalPrefrabs[animalIndex].transform.rotation);
+    }
+        
+    
 }
